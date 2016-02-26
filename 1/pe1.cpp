03dc9a59edd80c0c5d11b2ad207b8e9f96dfeb0a
pankajsyngh <pankajy7276@gmail.com>
2016-02-26 13:29:49 -0500

    version_26_02_2016

--- 1/pe1.cpp
+++ 1/pe1.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <math.h>
+#include <valarray>
+
 using namespace std;
 
 
@@ -10,30 +12,66 @@ void returns_multiple_array( int* arr, int a, int N ){
 	}
 }
 
+int returns_arr_sum (int* arr, int max){
+	
+	int sum = 0;
+	for( int i = 0; i < max; i = i +1){
+		sum = sum + arr[i];
+	}
+	return sum;
+}
+
 int main() {
 	
 	// Get the input on outer bound //
 	cout << "Enter the upper bound (N) : \n";
 	int N;
-	cin >> N;		
+	cin >> N;	
+	N = N - 1;	
+	system("CLS");
+	// If the N is less than 3, the result is 0 //
+	if (N < 3){
+		cout << "Sum is " << 0 << "\n";
+	}
 	
-	// Get multiple arrays for 3 //
-	int size_arr_3 = floor(N/3);
-	int arr_3[size_arr_3] = {};
-	returns_multiple_array(arr_3, 3, N);
-	cout << arr_3[0];
+	if (N < 5 && N >= 3){
+		cout << "Sum is " << 3 << "\n";
+	}
 	
-	// Get multiple arrays for 5 //
-	int size_arr_5 = floor(N/5);
-	int arr_5 [size_arr_5] = {};
-	returns_multiple_array(arr_5, 5, N);
-	cout << arr_5[0];
+	if (N < 15 && N >= 5){
+		int size_arr_3 = floor(N/3);
+		int arr_3[size_arr_3] = {};
+		returns_multiple_array(arr_3, 3, N);
+		
+		int size_arr_5 = floor(N/5);
+		int arr_5 [size_arr_5] = {};
+		returns_multiple_array(arr_5, 5, N);
+		
+		int arr_3_sum = returns_arr_sum(arr_3, size_arr_3);
+		int arr_5_sum = returns_arr_sum(arr_5, size_arr_5);
 	
-	// Get multiple arrays for 15 //
-	int size_arr_15 = floor(N/15);
-	int arr_15 [size_arr_15] = {};
-	returns_multiple_array(arr_15, 15, N);
-	cout << arr_15[0];
-	//int a;
-	//a = returns_multiple_array(3,N);
+		int sum = arr_3_sum + arr_5_sum;
+		cout << "Sum is " << sum << "\n";
+	}
+	
+	if (N >= 15){
+		int size_arr_3 = floor(N/3);
+		int arr_3[size_arr_3] = {};
+		returns_multiple_array(arr_3, 3, N);
+		
+		int size_arr_5 = floor(N/5);
+		int arr_5 [size_arr_5] = {};
+		returns_multiple_array(arr_5, 5, N);
+		
+		int size_arr_15 = floor(N/15);
+		int arr_15 [size_arr_15] = {};
+		returns_multiple_array(arr_15, 15, N);
+		
+		int arr_3_sum = returns_arr_sum(arr_3, size_arr_3);
+		int arr_5_sum = returns_arr_sum(arr_5, size_arr_5);
+		int arr_15_sum = returns_arr_sum(arr_15, size_arr_15);
+	
+		int sum = arr_3_sum + arr_5_sum - arr_15_sum;
+		cout << "Sum is " << sum << "\n";
+	}
 }
