83eac9a826a060a469e7c756d4f91cd04d9cf768
pankajsyngh <pankajy7276@gmail.com>
2016-02-28 13:49:28 -0500

    updated problem 2

--- 2/pe2.cpp
+++ 2/pe2.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 // The main function calculates the sum of all the numbers which are the multiple of 3 and 5 //
-int main() {
+int main(){
 	
 	// Get the input on outer bound //
 	cout << "Enter the upper bound (N) : \n";
@@ -24,19 +24,10 @@ int main() {
 	n = floor((1.0/3.0)*log(0.5*(sqrt(5.0)*max_N + sqrt(5.0*max_N*max_N + 4)))/log(phi1));
 	
 	// The highest Fibonacci number //
-	int an = (1/sqrt(5.0))*(pow(phi1, 3*n) - pow(phi2, 3*n));
+	double an = (1/sqrt(5.0))*(pow(phi1, 3*n) - pow(phi2, 3*n));
 	
 	// the sum of the required sequence //
-	int sum = (1/sqrt(5.0))*((1 - pow(phi1, 3*n+3))/(1 - pow(phi1, 3))- (1 - pow(phi2, 3*n+3))/(1 - pow(phi2, 3)));
+	double sum = (1/sqrt(5.0))*((1.0 - pow(phi1, 3*n+3))/(1.0 - pow(phi1, 3))- (1.0 - pow(phi2, 3*n+3))/(1.0 - pow(phi2, 3)));
 	
-	cout << "The index of 4000000 in Fibonacci series is : " << sum;
-	
-	
-	// If N is greater than 5 but less than 15 //
-//	int multiple_3_sum = returns_arr_sum(3, floor(N/3));
-//	int multiple_5_sum = returns_arr_sum(5, floor(N/5));	
-//	int multiple_15_sum = returns_arr_sum(15, floor(N/15));
-//	
-//	int sum = multiple_3_sum + multiple_5_sum - multiple_15_sum;
-//	cout << "Sum is " << sum << "\n";
+	cout<< "The sum of even Fibonacci numbers less than " << max_N << " is : " << sum;
 }
