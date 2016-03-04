8c2c11f47cd0da850863c041f415acb5914052a1
pankajsyngh <pankajy7276@gmail.com>
2016-03-04 09:49:14 -0500

    Problem 3

--- 3/pe3.cpp
+++ 3/pe3.cpp
@@ -1,33 +1,52 @@
-#include <iostream>
-#include <math.h>
-
+#include <bits/stdc++.h>
 using namespace std;
+ 
+void SieveOfEratosthenes(int n, bool* prime)
+{
+    // Create a boolean array "prime[0..n]" and initialize
+    // all entries it as true. A value in prime[i] will
+    // finally be false if i is Not a prime, else true.
+ 
+    for (int p=2; p*p<=n; p++)
+    {
+        // If prime[p] is not changed, then it is a prime
+        if (prime[p] == true)
+        {
+            // Update all multiples of p
+            for (int i=p*2; i<=n; i += p)
+                prime[i] = false;
+        }
+    }
+}
 
-// The main function calculates the sum of all the numbers which are the multiple of 3 and 5 //
-int main(){
-	
-	// Get the input on outer bound //
-	cout << "Enter the upper bound (N) : \n";
-	int max_N;
-	cin >> max_N;	
+
+// Main function to obtain the highest prime factor of a given input number
+int main()
+{
+    // Get the input on number //
+	cout << "Enter the number (n) : \n";
+	int n;
+	cin >> n;	
 	system("CLS");
 	
-    // Initiate the variables //
-	int n;							//index of the highest Fibonacci number below max_N
-	double phi1, phi2;				//golden ratio and its inverse
+	// initiate an array to store the location of prime numbers
+    bool prime_arr[n+1];
+    memset(prime_arr, true, sizeof(prime_arr));
+    
+    // getting location of all the prime numbers
+    SieveOfEratosthenes(n, prime_arr);
+    
+    int max_prime_factor = 2;
+    
+    for (int p=2; p<= (int) n/2; p++){
+       if (prime_arr[p]){
+	   		if (n%p == 0){
+	   			max_prime_factor = p;	
+			}
+	   }
+    }
+    
+    cout << "largest prime factor of " << n << " is " << max_prime_factor;
 	
-	// assigning values to phi1 and phi2 //
-	phi1 = 0.5*(1 + sqrt (5));
-	phi2 = 0.5*(1 - sqrt (5));
-	
-	// getting the index of the Fibonacci number within the max_N //
-	n = floor((1.0/3.0)*log(0.5*(sqrt(5.0)*max_N + sqrt(5.0*max_N*max_N + 4)))/log(phi1));
-	
-	// The highest Fibonacci number //
-	double an = (1/sqrt(5.0))*(pow(phi1, 3*n) - pow(phi2, 3*n));
-	
-	// the sum of the required sequence //
-	int sum = (1/sqrt(5.0))*((1.0 - pow(phi1, 3*n+3))/(1.0 - pow(phi1, 3))- (1.0 - pow(phi2, 3*n+3))/(1.0 - pow(phi2, 3)));
-	
-	cout<< "The sum of even Fibonacci numbers less than " << max_N << " is : " << sum;
+    return 0;
 }
