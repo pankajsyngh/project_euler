f64b722b5d086b30d9a21e09096c3e37b0f41d6b
pankajsyngh <pankajy7276@gmail.com>
2016-03-25 10:05:20 -0400

    Project Euler Problem 16

---
+++ 16/pe16.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <sstream>
+#include <gmpxx.h>
+
+using namespace std;
+
+
+int main () {
+
+	mpz_class number = pow(2,1000);
+	
+//	cout << number << endl;
+	
+	mpz_class sum = 0;
+	
+	while (number != 0){
+		
+		sum = sum + (number%10);
+		number = (number - (number%10))/10;
+	}
+	cout << sum << endl;
+		
+}
