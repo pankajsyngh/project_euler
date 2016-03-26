2f8ef7d19d5662a0b3928290c311ab248d621398
pankajsyngh <pankajy7276@gmail.com>
2016-03-26 12:18:10 -0400

    Project Euler problem 24

--- 24/pe24.cpp
+++ 24/pe24.cpp
@@ -1,1 +1,43 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <sstream>
+#include <gmpxx.h>
 
+using namespace std;
+
+int64_t factorial(int n){
+
+	int64_t fact = 1;
+	
+	for (int i = 1; i<=n; i++){
+	
+		fact = fact*i;
+	}
+	
+	return fact;
+}
+
+
+int main () {
+
+	vector<int> arr(8);
+	int64_t var, sum;
+	int j;
+	sum = 0;
+	
+	for (int i = 0; i < 8; i++){
+	
+		var = factorial(9-i);
+		j = 1;
+	
+		while (j*var + sum <= 1000000){
+		
+			j++;
+		}
+		
+		arr[i] = j - 1;
+		sum = sum + var*arr[i];
+		
+		cout << arr[i] << endl;
+	}
+}
